2606e2c5111e1c3086f0f04de46b4ce229ca4615
Thushen Mohanarajah <thushenarriyam@gmail.com>
2024-12-01 17:41:32 -0500

    Small change again: BottomNavigationItemUiContent

--- app/src/main/java/com/example/spendwise/provider/BottomNavigationItemProvider.kt
+++ app/src/main/java/com/example/spendwise/provider/BottomNavigationItemProvider.kt
@@ -4,9 +4,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.automirrored.outlined.List
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.outlined.Add
-import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Face
 import com.example.spendwise.model.BottomNavigationItemUiContent
 import com.example.spendwise.navigation.NavigationRoute
 
@@ -14,8 +14,8 @@ object BottomNavigationItemProvider {
     fun getItems(): List<BottomNavigationItemUiContent> = listOf(
         BottomNavigationItemUiContent(
             title = "Summary",
-            selectedIcon = Icons.Filled.Person,
-            unselectedIcon = Icons.Outlined.Person,
+            selectedIcon = Icons.Filled.Face,
+            unselectedIcon = Icons.Outlined.Face,
             navigationRoute = NavigationRoute.HomeScreen,
         ),
         BottomNavigationItemUiContent(
