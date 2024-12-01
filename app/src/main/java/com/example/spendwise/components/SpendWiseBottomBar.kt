6b3d6f23596eebec44ef9859289fff71e69bd3e7
Thushen Mohanarajah <thushenarriyam@gmail.com>
2024-11-30 23:15:22 -0500

    Created SpendWiseBottomBar and SpendWiseTopAppBar

---
+++ app/src/main/java/com/example/spendwise/components/SpendWiseBottomBar.kt
@@ -0,0 +1,77 @@
+package com.example.spendwise.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.automirrored.outlined.List
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.spendwise.NavigationRoute
+
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val navigationRoute: NavigationRoute,
+)
+
+@Composable
+fun SpendWiseBottomBar(modifier: Modifier = Modifier, navigateToScreen: (NavigationRoute) -> Unit) {
+    val items = listOf(
+        BottomNavigationItem(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            navigationRoute = NavigationRoute.HomeScreen,
+        ),
+        BottomNavigationItem(
+            title = "List",
+            selectedIcon = Icons.AutoMirrored.Filled.List,
+            unselectedIcon = Icons.AutoMirrored.Outlined.List,
+            navigationRoute = NavigationRoute.ListScreen,
+        ),
+        BottomNavigationItem(
+            title = "Settings",
+            selectedIcon = Icons.Filled.Settings,
+            unselectedIcon = Icons.Outlined.Settings,
+            navigationRoute = NavigationRoute.SettingScreen,
+        ),
+    )
+    var selectedItemIndex by remember { mutableIntStateOf(0) }
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = selectedItemIndex == index,
+                onClick = {
+                    selectedItemIndex = index
+                    navigateToScreen(item.navigationRoute)
+                },
+                label = {
+                    Text(text = item.title)
+                },
+                alwaysShowLabel = false,
+                icon = {
+                    Icon(
+                        imageVector = if (index == selectedItemIndex) {
+                            item.selectedIcon
+                        } else item.unselectedIcon,
+                        contentDescription = item.title
+                    )
+                }
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/spendwise/components/SpendWiseTopAppBar.kt
@@ -0,0 +1,23 @@
+package com.example.spendwise.components
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SpendWiseTopAppBar(modifier: Modifier = Modifier) {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Text("SpendWise")
+        }
+    )
+}
