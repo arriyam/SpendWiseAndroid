f94cc52c402d67ac1169a3417a1b2ff22e4f8179
Thushen Mohanarajah <thushenarriyam@gmail.com>
2024-11-30 23:14:57 -0500

    Created navigation

--- app/src/main/java/com/example/spendwise/MainActivity.kt
+++ app/src/main/java/com/example/spendwise/MainActivity.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.spendwise.ui.theme.SpendWiseTheme
+import com.example.spendwise.view.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +16,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SpendWiseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = NavigationRoute.HomeScreen) {
+                    composable<NavigationRoute.HomeScreen> {
+                        HomeScreen()
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SpendWiseTheme {
-        Greeting("Android")
-    }
+sealed interface NavigationRoute {
+    data object HomeScreen : NavigationRoute
+    data object ListScreen : NavigationRoute
+    data object SettingScreen : NavigationRoute
 }
