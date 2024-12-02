ae304f1c3e19c8657ab54d41fcecd431c431cd09
Thushen Mohanarajah <thushenarriyam@gmail.com>
2024-12-02 01:57:07 -0500

    Fixed alignment for top app bar title

--- app/src/main/java/com/example/spendwise/components/SpendWiseTopAppBar.kt
+++ app/src/main/java/com/example/spendwise/components/SpendWiseTopAppBar.kt
@@ -1,10 +1,10 @@
 package com.example.spendwise.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,13 +13,17 @@ import androidx.compose.ui.text.style.TextAlign
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SpendWiseTopAppBar(modifier: Modifier = Modifier) {
-    TopAppBar(
-        colors = TopAppBarDefaults.topAppBarColors(
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
             titleContentColor = MaterialTheme.colorScheme.primary,
         ),
         title = {
-            Text("SpendWise", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+            Text(
+                "SpendWise",
+                modifier = modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
         },
     )
 }
