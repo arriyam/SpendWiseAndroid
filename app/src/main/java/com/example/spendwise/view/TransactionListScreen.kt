a1a0dcca6d6b6b86daf643baa1bf9e42f093d0f6
Thushen Mohanarajah <thushenarriyam@gmail.com>
2024-12-02 02:20:50 -0500

    Minor changes 2 to TransactionListScreen.kt

--- app/src/main/java/com/example/spendwise/view/TransactionListScreen.kt
+++ app/src/main/java/com/example/spendwise/view/TransactionListScreen.kt
@@ -1,12 +1,21 @@
 package com.example.spendwise.view
 
-import android.widget.Toast
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.spendwise.components.CategoryDropdown
